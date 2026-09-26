1d69b3284e79cdbab8dbc6babc2fb4ffc7ad8922
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2026-09-27 01:33:43 +0530

    feat(desktop): expose JARVIS capability layer

---
+++ packages/desktop/src/jarvis/index.js
@@ -0,0 +1,6 @@
+export { resolveCommand, listCommands, requiresConfirmation } from './command-registry.js';
+export { CommandHistory } from './command-history.js';
+export { getSystemSnapshot } from './system-monitor.js';
+export { PersonalityManager, DEFAULT_PERSONALITY } from './personality-manager.js';
+export { ReminderManager } from './reminder-manager.js';
+export { ProactiveEngine } from './proactive-engine.js';
